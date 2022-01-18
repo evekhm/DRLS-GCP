997b84cdbe08b5b16c6a40fd0f1df33d1e8e2f5d
evekhm <evekhm@gmail.com>
2022-01-18 14:39:58 -0800

    Script to apply/rollout all worker components

---
+++ apply_workers.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -u # This prevents running the script if any of the variables have not been set
+set -e # Exit if error is detected during pipeline execution
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function apply_worker(){
+  "$DIR/$1/gcp/apply.sh"
+}
+
+apply_worker auth
+apply_worker CRD
+apply_worker dtr
+apply_worker test-ehr
+apply_worker crd-request-generator
+
+
+
+
---
+++ rollout_workers.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -u # This prevents running the script if any of the variables have not been set
+set -e # Exit if error is detected during pipeline execution
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function rollout_worker(){
+  "$DIR/$1/gcp/rollout.sh"
+}
+
+rollout_worker auth
+rollout_worker CRD
+rollout_worker dtr
+rollout_worker test-ehr
+rollout_worker crd-request-generator
+
+
+
+
