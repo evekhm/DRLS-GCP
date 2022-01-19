1f5b122f7c40c2b390150ba5d75effb3fbe5dbe4
evekhm <evekhm@gmail.com>
2022-01-19 12:32:17 -0800

    Support for GitLab Repo, additional scripts

--- build_images.sh
+++ build_images.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -u # This prevents running the script if any of the variables have not been set
 set -e # Exit if error is detected during pipeline execution
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"$DIR"/get_projects.sh
-"$DIR"/setup_cluster.sh
-"$DIR"/deploy_services.sh
-"$DIR"/apply_workers.sh
+function build_image(){
+  "$DIR/$1/bin/docker_build"
+}
 
-echo 'You are all Done! Use following External ips to access deployed services'
-cat DRLS-GCP/.env
+build_image auth
+build_image CRD
+build_image dtr
+build_image test-ehr
+build_image crd-request-generator
+
+
+
+
--- push_images.sh
+++ push_images.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -u # This prevents running the script if any of the variables have not been set
 set -e # Exit if error is detected during pipeline execution
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"$DIR"/get_projects.sh
-"$DIR"/setup_cluster.sh
-"$DIR"/deploy_services.sh
-"$DIR"/apply_workers.sh
+function push_image(){
+  "$DIR/$1/bin/docker_push"
+}
 
-echo 'You are all Done! Use following External ips to access deployed services'
-cat DRLS-GCP/.env
+push_image auth
+push_image CRD
+push_image dtr
+push_image test-ehr
+push_image crd-request-generator
+
+
+
+
