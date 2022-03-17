57ea3909e7817644868c689e989156bbe25e33d9
evekhm <evekhm@gmail.com>
2022-03-17 14:27:38 -0700

    replaced bash

--- jobs/build_keycloak.sh
+++ jobs/build_keycloak.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e # Exit if error is detected during pipeline execution
 JOBS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # File from the previous step with Service IPs
 echo "Running $(basename "$0") VARIABLES_FILE=$VARIABLES_FILE ..."
 PWD=$(pwd)
 
-echo "-------"
 #Keycloak needs TEST_EHR embedded in the image
 cat "${VARIABLES_FILE}"
 source "${VARIABLES_FILE}"
