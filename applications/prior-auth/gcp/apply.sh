fffbca9b29a565d19d66fe6815fccbcc8e5606a6
evekhm <evekhm@gmail.com>
2022-04-06 18:54:17 -0700

    added pa NEW version

---
+++ applications/prior-auth/gcp/apply.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e # Exit if error is detected during pipeline execution
+
+GCP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BIN="$GCP"/../bin
+PWD=$(pwd)
+source "$BIN"/SET
+
+
+echo "$(basename "$0") APPLICATION=$APPLICATION, IMAGE=$IMAGE KUBE_NAMESPACE=$KUBE_NAMESPACE"
+cd "$GCP"/../k8s/
+
+kubectl apply -f config.yaml --namespace="$KUBE_NAMESPACE"
+
+sed 's|__IMAGE__|'"$IMAGE"'|g;' deployment.sample.yaml > deployment.yaml
+kubectl apply -f deployment.yaml --namespace="$KUBE_NAMESPACE"
+
+kubectl apply -f service.yaml --namespace="$KUBE_NAMESPACE"
+
+cd "$PWD" || exit
+
+
---
+++ applications/prior-auth/gcp/delete.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e # Exit if error is detected during pipeline execution
+
+GCP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BIN="$GCP"/../bin
+PWD=$(pwd)
+source "$BIN"/SET
+
+gcloud container clusters get-credentials "$CLUSTER" --region="$REGION" --project "$PROJECT_ID"
+
+echo "***** Deleting $APPLICATION deployment at cluster $CLUSTER *****"
+cd "$GCP"/../k8s/ || exit
+
+if [ -f deployment.yaml ]; then
+  kubectl delete -f deployment.yaml
+fi
+
+kubectl delete -f service.yaml
+
+cd "$PWD" || exit
+
+echo "***** Deleted deployment for $APPLICATION ! *****"
+
+
