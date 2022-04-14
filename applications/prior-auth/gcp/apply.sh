2366fdbc6a485d8e52f844c72ccce641cba6d8c3
evekhm <evekhm@gmail.com>
2022-04-13 19:44:31 -0700

    Supporting PRIOR-AUTH

--- applications/prior-auth/gcp/apply.sh
+++ applications/prior-auth/gcp/apply.sh
@@ -11,9 +11,17 @@ source "$BIN"/SET
 echo "$(basename "$0") APPLICATION=$APPLICATION, IMAGE=$IMAGE KUBE_NAMESPACE=$KUBE_NAMESPACE"
 cd "$GCP"/../k8s/
 
+sed 's|__PROJECT_ID__|'"$PROJECT_ID"'|g;
+      s|__BUCKET__|'"$BUCKET_NAME"'|g;
+      s|__PRIOR_AUTH__|'"$PRIOR_AUTH"'|g;
+      s|__DB__|'"$DB_NAME"'|g; ' config.sample.yaml > config.yaml
 kubectl apply -f config.yaml --namespace="$KUBE_NAMESPACE"
 
-sed 's|__IMAGE__|'"$IMAGE"'|g;' deployment.sample.yaml > deployment.yaml
+sed 's|__KSA_NAME__|'"$KSA_NAME"'|g; ' serviceaccount.sample.yaml > serviceaccount.yaml
+kubectl apply -f serviceaccount.yaml --namespace="$KUBE_NAMESPACE"
+
+sed 's|__IMAGE__|'"$IMAGE"'|g;
+ s|__KSA_NAME__|'"$KSA_NAME"'|g; '  deployment.sample.yaml > deployment.yaml
 kubectl apply -f deployment.yaml --namespace="$KUBE_NAMESPACE"
 
 kubectl apply -f service.yaml --namespace="$KUBE_NAMESPACE"
