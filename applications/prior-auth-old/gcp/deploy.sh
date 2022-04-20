864d6fe76531cdd38ec30ebbb9edd3c9fcd785cc
evekhm <evekhm@gmail.com>
2022-04-19 22:51:32 -0700

    Removed pa-old, fixed config

--- applications/prior-auth-old/gcp/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-GCP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-UTILS="$GCP"/../../../shared
-BIN="$GCP"/../bin
-
-"$UTILS"/deploy "$BIN" "$GCP"
-
-
--- applications/prior-auth-old/gcp/rollout.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-GCP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BIN="$GCP"/../bin
-"$GCP"/../../../shared/rollout "$BIN" "$GCP"
