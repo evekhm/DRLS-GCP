c4604cd0eaf9c280d24aaffb3ba188a81e008fe8
evekhm <evekhm@gmail.com>
2022-04-22 12:39:58 -0700

    fixing SERVICE end-points

--- gitlab-ci/deploy.sh
+++ gitlab-ci/deploy.sh
@@ -11,8 +11,16 @@ echo "Running deploy step with following parameters APPLICATON=$APPLICATION,
 apt-get update && apt-get install git
 apt-get install zip unzip -q
 
+if [ -f "${VARIABLES_FILE}" ]; then
+  cat "${VARIABLES_FILE}"
+fi
 source "${VARIABLES_FILE}"
 source "${DIR}/../shared/vars"
+
+"${DIR}"/../jobs/get_services.sh -o "${DIR}"/.env
+cat "${DIR}"/.env
+source "${DIR}"/.env
+rm -rf "${DIR}"/.env
 
 gcloud auth activate-service-account --key-file "${SERVICE_ACCOUNT_FILE}" --project="$PROJECT_ID"
 "${DIR}"/../jobs/deploy_application.sh -a "$APPLICATION"
