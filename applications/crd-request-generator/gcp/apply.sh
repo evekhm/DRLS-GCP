7ac11ccac3b5ca6da724e987a756f60279b480f3
evekhm <evekhm@gmail.com>
2022-07-07 14:30:00 -0700

    Added launch_url config

--- applications/crd-request-generator/gcp/apply.sh
+++ applications/crd-request-generator/gcp/apply.sh
@@ -12,6 +12,7 @@ cd "$GCP"/../k8s/
 sed 's|__FHIR_SERVER__|'"$FHIR_SERVER"'|g;
     s|__AUTH__|'"$AUTH"'|g; # TODO replace __TOKEN__
     s|__PUBLIC_KEYS__|'"$PUBLIC_KEYS"'|g;
+    s|__DTR__|'"$DTR"'|g;
     s|__CDS_SERVICE__|'"$CDS_SERVICE"'|g;' config.sample.yaml > config.yaml
 kubectl apply -f config.yaml --namespace="$KUBE_NAMESPACE"
 
